959d24b0fcc71d768c3a29cec40cf21829a669a8
Noah Tajalli <noahtajalli@outlook.com>
2023-08-14 21:45:18 -0400

    First!

---
+++ src/script.js
@@ -0,0 +1,31 @@
+const typeEffectElement = document.getElementById("typeEffect");
+const words = ["Innovator", "Software Engineer", "Computer Scientist", "Mathematician", "Problem-Solver", "Creative Thinker", "technologist"];
+let wordIndex = 0;
+let letterIndex = 0;
+
+function typeEffect() {
+    if (letterIndex < words[wordIndex].length) {
+      document.getElementById("cursor").style.visibility = "visible"; // Show cursor
+      typeEffectElement.textContent += words[wordIndex][letterIndex];
+      letterIndex++;
+      setTimeout(typeEffect, 100);
+    } else {
+      document.getElementById("cursor").style.visibility = "hidden"; // Hide cursor during erase
+      setTimeout(eraseEffect, 100);
+    }
+  }
+  
+  function eraseEffect() {
+    if (letterIndex > 0) {
+      typeEffectElement.textContent = words[wordIndex].substring(0, letterIndex - 1);
+      letterIndex--;
+      setTimeout(eraseEffect, 100);
+    } else {
+      document.getElementById("cursor").style.visibility = "visible"; // Show cursor again
+      wordIndex = (wordIndex + 1) % words.length;
+      setTimeout(typeEffect, 100);
+    }
+  }
+  
+
+typeEffect();
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/script.js', // Update with your entry file
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Update with your HTML file
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+};
