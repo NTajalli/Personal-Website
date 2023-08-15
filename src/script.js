954d980f33ea06463bd57c1a01cded33b016bb3e
Noah Tajalli <noahtajalli@outlook.com>
2023-08-15 15:54:52 -0400

    Mobile

--- src/script.js
+++ src/script.js
@@ -34,3 +34,13 @@ function typeEffect() {
   
 
 typeEffect();
+
+document.getElementById('hamburger-btn').addEventListener('click', function() {
+  var navbarLinks = document.getElementById('navbar-links');
+
+  if (navbarLinks.style.display === 'none' || navbarLinks.style.display === '') {
+    navbarLinks.style.display = 'block';
+  } else {
+    navbarLinks.style.display = 'none';
+  }
+});
